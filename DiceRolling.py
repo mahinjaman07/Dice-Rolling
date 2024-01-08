5e718d2f7230630969891388d6da115ca87f9591
mahinjaman01 <mahinjaman434@gmail.com>
2024-01-08 11:48:08 +0600

    Dice Rolling Game In Python

---
+++ DiceRolling.py
@@ -0,0 +1,14 @@
+import random
+
+DiceRolling = True;
+
+while DiceRolling:
+    answer = input("Do You Want To Rolling Again [Y/n] :").lower();
+    if answer == "y" or answer == "yes":
+        DiceNumber = random.randint(1, 6);
+        print(f"Your Dice Number is {DiceNumber}");
+        continue;
+
+    else:
+        print("Game Over");
+        break;
